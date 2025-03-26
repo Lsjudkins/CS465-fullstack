62e17b792531d53735a62a115d6b83ced9d03c75
Lsjudkins <latoria.judkins@snhu.edu>
2025-03-26 13:32:55 -0400

    Module 3 Completed Baseline

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,7 +1,9 @@
+var fs = require('fs');
+var trips = JSON.parse(fs.readFileSync('./data/trips.json','utf8'));
+
 /* GET travel view */
 const travel = (req, res) => {
-    pageTitle = process.env.npm_package_decsription + ' - Travel';
-    res.render('travel', {title: 'pageTitle'});
+    res.render('travel', {title: 'Travlr Getaways', trips});
 };
 
 module.exports = {
