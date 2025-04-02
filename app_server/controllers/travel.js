ced055b14058c9a224b5e5baf382e46ceaf4ae49
Lsjudkins <latoria.judkins@snhu.edu>
2025-04-02 19:33:37 -0400

    Module 5 completed baseline

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,9 +1,25 @@
-var fs = require('fs');
-var trips = JSON.parse(fs.readFileSync('./data/trips.json','utf8'));
+const tripsEndpoint = 'http://localhost:3000/api/trips';
+const options = {
+    method: 'GET',
+    headers: {
+        'Accept': 'applications/json'
+    }
+}
+
+// var fs = require('fs');
+// var trips = JSON.parse(fs.readFileSync('./data/trips.json','utf8'));
 
 /* GET travel view */
-const travel = (req, res) => {
-    res.render('travel', {title: 'Travlr Getaways', trips});
+const travel = async function(req, res) {
+    // console.log('TRAVEL CONTROLLER BEGIN');
+    await fetch(tripsEndpoint, options)
+        .then(res => res.json())
+        .then(json => {
+            // console.log(json);
+            res.render('travel', {title: 'Travlr Getaways', trips: json});
+        })  
+        .catch(err => res.status(500).send(e.message));
+    // console.log('TRAVEL CONTROLLER AFTER RENDER);
 };
 
 module.exports = {
